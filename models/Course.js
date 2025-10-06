8f5919f110e5c2d78404e24a0090247e597561fe
rushdy87 <mohammed_alrushdy@yahoo.com>
2025-10-07 01:00:52 +0300

    Aggregate - Calculating The Average Course Cost.

--- models/Course.js
+++ models/Course.js
@@ -39,4 +39,59 @@ const CourseSchema = new Schema({
   },
 });
 
+// Static method to get avg of course tuitions
+// The static method in mongoose is called on the model itself, not on an instance of the model
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+  console.log("Calculating avg cost...");
+
+  try {
+    // The aggregate method is used to perform aggregation operations on the model's data
+    // It takes an array of stages that define the aggregation pipeline.
+    const obj = await this.aggregate([
+      {
+        $match: { bootcamp: bootcampId },
+      },
+      {
+        $group: {
+          _id: "$bootcamp",
+          averageCost: { $avg: "$tuition" },
+        },
+      },
+    ]);
+
+    // Check if there are any courses for this bootcamp
+    if (obj && obj.length > 0) {
+      await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+        averageCost: Math.ceil(obj[0].averageCost / 10) * 10,
+      });
+    } else {
+      // If no courses, set average cost to 0 or undefined
+      await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+        averageCost: undefined,
+      });
+    }
+  } catch (err) {
+    console.error("Error calculating average cost:", err);
+  }
+};
+
+// Call the method getAverageCost after save
+CourseSchema.post("save", async function () {
+  await this.constructor.getAverageCost(this.bootcamp);
+});
+
+// Call the method getAverageCost before remove
+CourseSchema.pre(
+  "deleteOne",
+  { document: true, query: false },
+  async function () {
+    await this.constructor.getAverageCost(this.bootcamp);
+  }
+);
+
+// Call the method getAverageCost after remove (for legacy remove method)
+CourseSchema.pre("remove", async function () {
+  await this.constructor.getAverageCost(this.bootcamp);
+});
+
 module.exports = mongoose.model("Course", CourseSchema);

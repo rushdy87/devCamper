180030c99b8606aace873b5a61202260b2be1b15
rushdy87 <mohammed_alrushdy@yahoo.com>
2025-10-06 09:57:48 +0300

    Git a single course

--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { getCourses } = require("../controllers/courses");
+const { getCourses, getCourse } = require("../controllers/courses");
 
 // Merge params to get access to bootcampId in the params
 const router = express.Router({ mergeParams: true });
 
 router.route("/").get(getCourses);
+router.route("/:id").get(getCourse);
 
 module.exports = router;
